c4abbeb3687cec02780056cc14924869ce03be99
hugoberra <jtberra@gmail.com>
2024-03-02 23:35:32 -0600

    Feat: functionality see municipalities
    body: aplicacion de funcionalidad para eliminar y modificar municipio

--- SMM_module/views.py
+++ SMM_module/views.py
@@ -22,3 +22,21 @@ def dashboard(request):
 def ver_municipios(request):
     municipios = Municipality.objects.all()
     return render(request, 'gestion_municipio/view_municipality/view_municipality.html', {'municipios': municipios}) #ver_municipios
+
+
+def eliminar_municipio(request, id):
+    municipio = Municipality.objects.get(id=id)
+    municipio.delete()
+    return redirect('ver_municipios')
+
+def modificar_municipio(request, id):
+    municipio = Municipality.objects.get(id=id)
+    if request.method == 'POST':
+        form = MunicipalityForm(request.POST, instance=municipio)
+        if form.is_valid():
+            form.save()
+            return redirect('ver_municipios')
+    else:
+        form = MunicipalityForm(instance=municipio)
+
+    return render(request, 'gestion_municipio/add_municipality/base.html', {'form': form})
