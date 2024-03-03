543444e2a9afc4b3394bfefc0b0c5bcc9dde7b42
hugoberra <jtberra@gmail.com>
2024-03-02 20:27:46 -0600

    Feat: routing model
    body: modelo de rrutas para el ejemplo de navegacion

--- SMM_module/views.py
+++ SMM_module/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def dashboard(request):
+    return render(request, 'dashboard/dashboard.html')
+
+def registrar_municipio(request):
+    # Lógica para el formulario de registro
+    return render(request, 'gestion_municipio/add_municipality/base.html') # registrar
+
+def ver_municipios(request):
+    # Lógica para mostrar los municipios
+    return render(request, 'gestion_municipio/mod_municipality/base.html') #ver_municipios
